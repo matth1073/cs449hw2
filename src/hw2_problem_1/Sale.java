dfb376c37f891ec8d656d922c70c2a1d2c5343f5
matth <matth@Indigo>
2019-10-21 11:20:20 -0500

    dsaDS

---
+++ src/hw2_problem_1/Sale.java
@@ -0,0 +1,33 @@
+package hw2_problem_1;
+
+import java.util.ArrayList;
+
+public class Sale {
+	private static double DISCOUNT_RATE = 0.05;
+	
+	private ArrayList<Item> itemList = new ArrayList<Item>();
+	
+	public void addItem(Item item) {
+		itemList.add(item);
+	}
+	public double getDiscountRate(){
+		return DISCOUNT_RATE;
+	}
+	public double getDiscount(){
+		return getSubTotal()*getDiscountRate();
+	}
+	
+	public double getSubTotal() {
+		double subTotal = 0.0;
+		for (Item item: itemList) {
+			subTotal += item.getItemTotal();
+		}
+		
+		return subTotal;
+	}
+	
+	public double getDiscountedSubTotal() {
+		return getSubTotal() - getDiscount();
+	}
+
+}
